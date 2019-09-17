55d3b19534d4b1e9383473dadc1ed51adfe88b51
Amy Avirut <amyavirut@gmail.com>
2019-09-17 23:18:30 +0700

    120201

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,49 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import axios from 'axios'
 
 class Login extends React.Component {
+    state = {
+        email: "",
+        password: ""
+    }
+    
+    handleInputChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        })
+    }
+
+    login = (e) => {
+        e.preventDefault()
+        let requiredFields = ['email', 'password']
+        let emptyFields = requiredFields.filter(f => this.state[f] === "")
+        if (emptyFields.length > 0) {
+            alert(`${emptyFields.join(",")} can't be empty`)
+            return
+        }
+        axios.post(`${process.env.REACT_APP_API_URL}/login`, this.state)
+            .then(res => {
+                let token = res.data
+                localStorage.setItem('token', token)
+                this.props.history.push({pathname: '/'})
+            }).catch(err =>
+                console.log(err)
+            )
+    }
     render() {
         return <div className="grid center middle tall image">
         <div className="card small">
             <div className="content">
                 <div className="logo" />
-                <form>
+                <form onSubmit={this.login}>
                     <div className="group">
                         <label>Email</label>
-                        <input type="email"/>
+                        <input name="email" onChange={this.handleInputChange} type="email" value={this.state.email} />
                     </div>
                     <div className="group">
                         <label>Password</label>
-                        <input type="password"/>
+                        <input name="password" onChange={this.handleInputChange} type="password" value={this.state.password} />
                     </div>
                     <button className="primary">Login</button>
                 </form>
